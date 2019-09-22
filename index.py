0cfc9255db31721cd30a36641628183af1de4c96
Daniel <dherskowitz@hertzfurniture.com>
2019-09-22 16:55:39 +0300

    prep for deploy

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import os
 import re
 import time
+import requests
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, render_template, request, redirect, jsonify
 from datetime import datetime
@@ -33,10 +34,22 @@ def index():
 def signup():
     email = request.form["email"]
 
+    # get user ip
+    user_ip = request.headers.get("X-Forwarded-For", request.remote_addr)
+
+    # Use my ip from env file for local testing
+    if user_ip == "127.0.0.1":
+        user_ip = os.getenv("MY_IP")
+
     # get location from ip
-    url = "http://ipinfo.io/json"
-    response = urlopen(url)
-    data = json.load(response)
+    ip_url_base = "http://ipinfo.io/" + user_ip
+    ip_url_prefs = "/geo?token=" + os.getenv("IPINFO_TOKEN")
+    ip_url = ip_url_base + ip_url_prefs
+    ip_response = requests.get(ip_url)
+    ip_data = ip_response.json()
+
+    print("=============")
+    print(ip_data)
 
     if email == "" or not re.match(email_pattern, email):
         response = {"response": "invalid_email"}
@@ -46,8 +59,8 @@ def signup():
             f"{datetime.now().month}/{datetime.now().day}/{datetime.now().year}"
         )
         signup_timestamp = f"{datetime.now().time()}"
-        signup_region = f"{data['region']}, {data['country']}"
-        signup_ip = f"{data['ip']}"
+        signup_region = f"{ip_data['city']}, {ip_data['region']}, {ip_data['country']}"
+        signup_ip = f"{ip_data['ip']}"
         signup_data = [email, signup_date, signup_timestamp, signup_region, signup_ip]
 
         # write data to sheet
