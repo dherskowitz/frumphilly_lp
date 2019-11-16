f50a6dc8c187ec55e73bc9bcb8c48a43466d54ec
Daniel <dherskowitz@hertzfurniture.com>
2019-11-16 21:42:52 +0200

    removed print statements

--- index.py
+++ index.py
@@ -37,9 +37,6 @@ def get_client_ip(request):
 
 @app.route("/")
 def index():
-    print("==============")
-    print(dir(request))
-    print(get_client_ip(request))
     filename = os.path.join(app.static_folder, "content.json")
     with open(filename) as content:
         data = json.load(content)
